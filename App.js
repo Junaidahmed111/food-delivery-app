b2aa0609dcccfa67ea5d8b27d4151d02ae367257
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-22 19:43:17 +0500

    fix

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello</Text>
+      <Text>hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
