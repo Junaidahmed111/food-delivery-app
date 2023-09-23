2385ceafab234bf38392632d425207825d010347
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-23 19:58:02 +0500

    useNavigation hook

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,5 +1,7 @@
 import { View, Text, Pressable, StyleSheet, Platform } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 function CategoryGridTile({ title, color, onPress }) {
+  const navigation = useNavigation();
   return (
     <View style={[styles.gridItem]}>
       <Pressable
