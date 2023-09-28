fb7453030b48b460dbac9d670759802c02e022d0
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-28 22:41:03 +0500

    fix

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default function App() {
         <Stack.Navigator
           screenOptions={{
             headerStyle: { backgroundColor: "#702727" },
-            headerTintColor: "black",
+            headerTintColor: "white",
             contentStyle: { backgroundColor: "#b87f7f" },
           }}
         >
