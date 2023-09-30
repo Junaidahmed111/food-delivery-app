67b142472ee8ac3191453f0c76a72f607815cc78
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-09-30 23:43:06 +0500

    fix

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     height: 150,
     borderRadius: 8,
     elevation: 4,
-    backgroundColor: "black",
+    backgroundColor: "white",
     shadowColor: "black",
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
