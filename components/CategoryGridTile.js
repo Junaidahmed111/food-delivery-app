39650fd629188653276564dd0d65a9fd07d60e1d
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-01 23:55:11 +0500

    fix

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     shadowColor: "black",
     shadowOpacity: 0.25,
-    shadowOffset: { width: 0, height: 3 },
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     overflow: Platform.OS === "android" ? "hidden" : "visible",
   },
